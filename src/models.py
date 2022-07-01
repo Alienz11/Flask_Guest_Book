c45b8fc2dac4f856d48a79cd79e87a385040c6eb
Kenechukwu Nsofor <nsoforkenechukwu2@gmail.com>
2022-07-01 22:21:03 +0100

    :gear: Database model creator

---
+++ src/models.py
@@ -0,0 +1,15 @@
+from . import db
+from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import SelectField
+
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(40))
+    phone_no = db.Column(db.String(20))
+    floor = db.Column(db.Integer)
+    company = db.Column(db.String(40))
+    whom_to_see = db.Column(db.String(40))
+    comment = db.Column(db.String(1500))
+    date_in = db.Column(db.DateTime, default=datetime.utcnow)
